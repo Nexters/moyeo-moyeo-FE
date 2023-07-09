622cb3f762a8c9174b5c7695ba45f2b309cd395b
leeharyung <lhr4884@naver.com>
2023-07-09 15:56:45 +0900

    chore: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
   },
   plugins: ['react-refresh'],
   rules: {
