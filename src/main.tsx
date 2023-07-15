255af1ed1c380d3d5793a1bc5265a3a8e53bf491
young-do <yd199323@gmail.com>
2023-07-16 01:57:32 +0900

    css 파일 하나의 폴더로 옮기기

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-import './font.css';
-import './index.css';
+import './styles/font.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
