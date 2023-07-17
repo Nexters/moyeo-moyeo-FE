eff444bec694f5790b01b81256f0660954e81cc3
leeharyung <lhr4884@naver.com>
2023-07-18 01:24:31 +0900

    feat: add entry page

--- src/pages/Room.tsx
+++
@@ -1,5 +0,0 @@
-const Room = () => {
-  return <div>방 입장 페이지</div>;
-};
-
-export default Room;
---
+++ src/pages/Room/Admin.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Admin = () => {
+  return <div>Admin</div>;
+};
