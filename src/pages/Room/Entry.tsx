8f136e2fe408c468b72eef313f3274fb85b712d6
leeharyung <lhr4884@naver.com>
2023-07-19 00:35:55 +0900

    feat: add selected team id

--- src/pages/Room/Entry.tsx
+++ src/pages/Room/Entry.tsx
@@ -4,20 +4,28 @@ import { Button } from '@/components/Button';
 import { mockTeams } from '@/mock/data';
 import { css } from '@/styled-system/css';
 import { container, vstack } from '@/styled-system/patterns';
+import { Team } from '@/types';
 
 type EntryProps = {
   handleRole: (role: 'admin' | 'player') => void;
+  setTeamId: (teamId: Team['id']) => void;
 };
 
 const ROOM_NAME = 'NEXTERS 23기 팀 빌딩 - 방 제목';
 
-export const Entry = ({ handleRole }: EntryProps) => {
-  const [selectedTeam, setSelectedTeam] = useState<string | undefined>();
+export const Entry = ({ handleRole, setTeamId }: EntryProps) => {
+  const [selectedTeam, setSelectedTeam] = useState<
+    Team['pmName'] | undefined
+  >();
   const teams = mockTeams.map((team) => team.pmName);
 
   const handleChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     e.preventDefault();
     setSelectedTeam(e.target.value);
+    setTeamId(
+      mockTeams.find((team) => team.pmName === e.target.value)
+        ?.id as Team['id'],
+    );
   };
 
   return (
--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import { Team } from '@/types';
 import { isValidRoomId } from '@/utils/room';
 
 import { Admin } from './Admin';
@@ -12,6 +13,7 @@ import { Player } from './Player';
 const Room = () => {
   const [role, setRole] = useState<'admin' | 'player' | null>();
   const [exist, setExist] = useState<boolean>();
+  const [teamId, setTeamId] = useState<Team['id'] | null>();
   const { roomId } = useParams();
 
   useEffect(() => {
@@ -20,8 +22,8 @@ const Room = () => {
   }, [roomId]);
 
   if (!exist) return <NotFound />;
-  if (!role) return <Entry handleRole={setRole} />;
-  return role === 'player' ? <Player /> : <Admin />;
+  if (!role) return <Entry handleRole={setRole} setTeamId={setTeamId} />;
+  return role === 'player' ? <Player teamId={teamId!} /> : <Admin />;
 };
 
 export default Room;
