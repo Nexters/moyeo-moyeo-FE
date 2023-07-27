2ed38abd22b7f980556d0350d7e41257f5177c2b
leeharyung <lhr4884@naver.com>
2023-07-28 07:22:31 +0900

    chore: add emotion, chakra

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 });
