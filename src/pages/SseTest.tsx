358c8b63b574583c53e990c3777b2e272a01f0b4
young-do <yd199323@gmail.com>
2023-07-31 17:47:17 +0900

    주석 조금 더 추가

--- src/pages/SseTest.tsx
+++ src/pages/SseTest.tsx
@@ -8,7 +8,7 @@ const SseTest = () => {
   const setEventSource = useSetAtom(eventSourceAtom);
 
   useEffect(() => {
-    // root component에서 EventSource를 생성하고, atom에 저장한다.
+    // @note: root component에서 EventSource를 생성하고, atom에 저장한다.
     const eventSource = new EventSource(`http://localhost:8080/sse`);
     setEventSource(eventSource);
     return () => eventSource.close();
@@ -28,9 +28,9 @@ const SubComponent = () => {
   const eventSource = useAtomValue(eventSourceAtom);
 
   useEffect(() => {
-    // 개별 컴포넌트에서 EventSource에 이벤트 리스너를 설정한다.
+    // @note: 개별 하위 컴포넌트에서 EventSource에 이벤트 리스너를 설정한다.
     const handler = (e: MessageEvent<string>) => {
-      console.log(e.data);
+      console.log(e.data); // <- data는 위에 generic에 선언한 것 처럼 string이다.
     };
 
     eventSource?.addEventListener('message', handler);
