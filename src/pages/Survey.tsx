d2c085b34c91127fc33b2660df49f1c416ed0208
leeharyung <lhr4884@naver.com>
2023-08-01 15:37:43 +0900

    feat: add submit case

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -1,25 +1,14 @@
-import React, { Fragment, useRef, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import { toast } from 'react-hot-toast';
-import Select, {
-  ControlProps,
-  Props,
-  StylesConfig,
-  components,
-} from 'react-select';
+import Select from 'react-select';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { MAX_ROUND, POSITION } from '@/constants/game';
 import { mockTeams } from '@/mock/data';
 import { css } from '@/styled-system/css';
-import { container, stack, vstack } from '@/styled-system/patterns';
-
-const options = [
-  { value: '바나나', label: '바나나' },
-  { value: '사과', label: '사과' },
-  { value: '딸기', label: '딸기' },
-];
+import { stack } from '@/styled-system/patterns';
 
 const ROUND_ARRAY = Array.from({ length: MAX_ROUND }, (_, i) => i);
 
@@ -33,42 +22,40 @@ const teamOption = mockTeams.map((team) => ({
   label: team.id,
 }));
 
-type Option = {
-  value: string;
-  label: string;
-};
-
-const isDuplicateChoice =
-  (positions: string[]) => (currentRound: number, selectedPosition: string) => {
-    return positions
-      .filter((_, i) => i !== currentRound)
-      .includes(selectedPosition);
-  };
-
 const Survey = () => {
   const [inputs, setInputs] = useState({
-    name: '',
-    userPosition: '',
-    selectedPosition: Array.from({ length: MAX_ROUND }, () => ''),
+    userName: '',
+    position: '',
+    choices: Array.from({ length: MAX_ROUND }, () => ''),
   });
-  const selectInputRef = useRef<any>();
-  const [selectValue, setSelectValue] = useState('');
-
-  const handleClearSelect = () => {
-    if (selectInputRef.current) {
-      console.log(':: ', selectInputRef.current?.clearValue);
-      selectInputRef.current?.clearValue();
-    }
-  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
+    setInputs({ ...inputs, [name]: value.replace(/\s/g, '') });
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // FIXME
+    if (inputs.userName === '') {
+      toast.error('이름을 입력해주세요');
+      return;
+    }
+    if (inputs.position === '') {
+      toast.error('직군을 선택해주세요');
+      return;
+    }
+    if (inputs.choices.includes('')) {
+      toast.error('지망을 모두 선택해주세요');
+      return;
+    }
+    const isDuplicate = inputs.choices.some(
+      (team, i) => inputs.choices.indexOf(team) !== i,
+    );
+    if (isDuplicate) {
+      toast.error('팀은 중복되지 않게 선택해주세요');
+      return;
+    }
     console.log(inputs);
     toast.success('설문조사가 제출되었습니다');
   };
@@ -76,17 +63,19 @@ const Survey = () => {
   return (
     <div
       className={stack({
-        width: '600px',
-        backgroundColor: 'rgba(12, 13, 14, 0.6)',
+        width: '360px',
+        backgroundColor: 'rgba(16, 28, 41, 0.6)',
         borderRadius: '40px',
         padding: '24px',
-        gap: '12px',
+        gap: '10px',
+        marginX: '8px',
       })}
     >
       <h1
         className={css({
           fontSize: '24px',
           color: 'white',
+          fontWeight: '700',
         })}
       >
         넥스터즈 23기 팀 빌딩
@@ -104,65 +93,47 @@ const Survey = () => {
         placeholder="이름을 입력해주세요"
         name="name"
         id="name"
-        value={inputs.name}
+        maxLength={50}
+        value={inputs.userName}
         onChange={handleChange}
       />
       <label className={css({ color: 'white' })}>직군 *</label>
       <Select
         options={positionOption}
         onChange={(e) => {
-          setInputs({ ...inputs, userPosition: e?.value || '' });
+          setInputs({ ...inputs, position: e?.value || '' });
         }}
         placeholder="포지션을 선택해주세요"
       />
-
       {ROUND_ARRAY.map((round) => {
         return (
           <Fragment key={round}>
             <label className={css({ color: 'white' })}>{round + 1}지망 *</label>
             <Select
-              ref={selectInputRef}
               options={teamOption}
               placeholder={`${round + 1}지망을 선택해주세요`}
               onChange={(e) => {
-                console.log(':: ', e, inputs.selectedPosition[round]);
                 if (!e) return;
-                if (inputs.selectedPosition[round] === e.value) return;
-                if (inputs.selectedPosition.includes(e.value)) {
-                  toast.error('이미 선택한 포지션입니다');
-                  handleClearSelect();
-                  return;
-                }
                 setInputs({
                   ...inputs,
-                  selectedPosition: inputs.selectedPosition.map(
-                    (position, i) => {
-                      if (i === round) return e.value;
-                      return position;
-                    },
-                  ),
+                  choices: inputs.choices.map((position, i) => {
+                    if (i !== round) return position;
+                    return e.value;
+                  }),
                 });
               }}
-              theme={(theme) => ({ ...theme, color: 'red' })}
             />
           </Fragment>
         );
       })}
-      <Select
-        ref={selectInputRef}
-        onChange={(e) => {
-          if (e) {
-            setSelectValue(e.value);
-          }
-        }}
-        options={options}
-        placeholder="뚝딱을 선택하세요."
-      />
-      <button onClick={handleClearSelect}>clear</button>
       <Button
         onClick={handleSubmit}
         visual="blue"
-        className={css({ marginTop: '12px' })}
+        className={css({
+          margin: '0 auto',
+          marginTop: '12px',
+          width: 'fit-content',
+        })}
       >
         제출하기
       </Button>
