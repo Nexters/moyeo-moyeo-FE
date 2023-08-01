97c7d315b2b5fce8ab2ac74cde421e867d3bc236
halang <lhr4884@naver.com>
2023-08-01 19:41:31 +0900

    [MOYEO-37] 설문지 페이지 마크업 & React Select 적용 (#17)
    * feat: add survey
    
    * feat: add submit case
    
    * fix: change name
    
    * feat: add e type
    
    * chore: add react-select
    
    * fix: delete unused vars
    
    * rename: rename constants
    
    * fix: max length
    
    * fix: fix survey
    
    * design: fix bg
    
    * fix: add review

---
+++ src/utils/const.ts
@@ -0,0 +1,39 @@
+import { Round } from '@/types';
+
+export const MAX_ROUND = 4;
+
+export const ROUNDS: Round[] = [
+  '1지망',
+  '2지망',
+  '3지망',
+  '4지망',
+  '자유',
+  '종료',
+];
+
+export const ROUND_INDEX_MAP: Record<string, number> = {
+  '1지망': 0,
+  '2지망': 1,
+  '3지망': 2,
+  '4지망': 3,
+};
+
+export const NEXT_ROUND_MAP: Record<Round, Round> = {
+  '1지망': '2지망',
+  '2지망': '3지망',
+  '3지망': '4지망',
+  '4지망': '자유',
+  자유: '종료',
+  종료: '종료',
+};
+
+export const POSITION: Record<string, string> = {
+  frontend: '프론트엔드',
+  backend: '백엔드',
+  ios: 'iOS',
+  android: '안드로이드',
+  design: '디자인',
+};
+
+// max length
+export const MAX_LENGTH__USER_NAME = 50;
