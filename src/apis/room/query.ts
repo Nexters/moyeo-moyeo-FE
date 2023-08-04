4c33a9db716418849408d0afdc0bf11b063883e1
leeharyung <lhr4884@naver.com>
2023-08-04 17:28:40 +0900

    fix: api를 query 파일로 합치기

--- src/apis/room/query.ts
+++ src/apis/room/query.ts
@@ -1,7 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { roomInfo } from '@/apis/room/api';
+import { getAsync } from '@/apis/fetch';
 import { type RoomInfo } from '@/apis/room/type';
+
+export const roomInfo = {
+  get: async ({ roomId }: RoomInfo['get']['request']) => {
+    const data = await getAsync<RoomInfo['get']['response']>(
+      `rooms/${roomId}/info`,
+    );
+    return data;
+  },
+};
 
 export const useGetRoomInfo = (params: RoomInfo['get']['request']) => {
   return useQuery(['roomInfo'], () => roomInfo.get({ ...params }));
