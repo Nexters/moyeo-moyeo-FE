e33c8cd20daa045edb781efda3068c99a8702747
young-do <yd199323@gmail.com>
2023-08-04 17:03:30 +0900

    fetch 함수들 httpClient로 제공하는 방식 변경

--- src/apis/room/api.ts
+++ src/apis/room/api.ts
@@ -1,9 +1,9 @@
-import { getAsync } from '@/apis/fetch';
+import { httpClient } from '@/apis/http';
 import { type RoomInfo } from '@/apis/room/type';
 
 export const roomInfo = {
   get: async ({ roomId }: RoomInfo['get']['request']) => {
-    const data = await getAsync<RoomInfo['get']['response']>(
+    const data = await httpClient.get<RoomInfo['get']['response']>(
       `rooms/${roomId}/info`,
     );
     return data;
