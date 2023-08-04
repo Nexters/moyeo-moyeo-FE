0215f373987a2c29f9dcd2f028b12e2523481d21
leeharyung <lhr4884@naver.com>
2023-08-04 17:26:39 +0900

    fix: props를 params으로 변경

--- src/apis/room/query.ts
+++ src/apis/room/query.ts
@@ -3,6 +3,6 @@ import { useQuery } from '@tanstack/react-query';
 import { roomInfo } from '@/apis/room/api';
 import { type RoomInfo } from '@/apis/room/type';
 
-export const useGetRoomInfo = (props: RoomInfo['get']['request']) => {
-  return useQuery(['roomInfo'], () => roomInfo.get({ ...props }));
+export const useGetRoomInfo = (params: RoomInfo['get']['request']) => {
+  return useQuery(['roomInfo'], () => roomInfo.get({ ...params }));
 };
