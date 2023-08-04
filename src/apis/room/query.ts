b083dd4bffb748e2e43e784200b0295326f0a55b
leeharyung <lhr4884@naver.com>
2023-08-04 18:50:52 +0900

    fix: type 네이밍 수정

--- src/apis/room/query.ts
+++ src/apis/room/query.ts
@@ -1,17 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { httpClient } from '@/apis/http';
-import { type RoomInfo } from '@/apis/room/type';
+import { type API } from '@/apis/room/type';
 
-export const roomInfo = {
-  get: async ({ roomId }: RoomInfo['get']['request']) => {
-    const data = await httpClient.get<RoomInfo['get']['response']>(
+export const totalInfo = {
+  get: async ({ roomId }: API['totalInfo']['get']['request']) => {
+    const data = await httpClient.get<API['totalInfo']['get']['response']>(
       `rooms/${roomId}/info`,
     );
     return data;
   },
 };
 
-export const useGetRoomInfo = (params: RoomInfo['get']['request']) => {
-  return useQuery(['roomInfo'], () => roomInfo.get({ ...params }));
+export const useGetTotalInfo = (params: API['totalInfo']['get']['request']) => {
+  return useQuery(['roomInfo'], () => totalInfo.get({ ...params }));
 };
--- src/apis/room/type.ts
+++ src/apis/room/type.ts
@@ -1,14 +1,55 @@
-export type RoomInfo = {
-  get: {
-    request: {
-      roomId: string;
-    };
-    response: {
-      uuid: string;
-      teamName: string;
-      pmName: string;
-      pmPosition: string;
-      selectDone: boolean;
+export type Round =
+  | 'FIRST_ROUND'
+  | 'SECOND_ROUND'
+  | 'THIRD_ROUND'
+  | 'FOURTH_ROUND'
+  | 'ADJUSTED_ROUND'
+  | 'COMPLETE';
+
+export type Position =
+  | 'DESIGNER'
+  | 'BACK_END'
+  | 'FRONT_END'
+  | 'IOS'
+  | 'ANDROID';
+
+export type Room = {
+  roomUrl: string;
+  roundStatus: Round;
+};
+
+export type Team = {
+  uuid: string;
+  teamName: string;
+  pmPosition: Position;
+  selectDone: boolean;
+};
+
+export type User = {
+  uuid: string;
+  userName: string;
+  position: Position;
+  choices: string[];
+  joinedTeamId: string;
+  selectedTeam: boolean;
+};
+
+export type TotalInfo = {
+  roomInfo: Room;
+  teamInfoList: Team[];
+  userInfoList: User[];
+};
+
+// TODO: 추후 위 타입들은 types/index.ts로 분리
+// TODO: 아래 API 타입도 depth 줄이기
+
+export type API = {
+  totalInfo: {
+    get: {
+      request: {
+        roomId: string;
+      };
+      response: TotalInfo;
     };
   };
 };
