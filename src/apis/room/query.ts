2aa6b2bdb3e265efb6fb55100de169f1f9b5403f
leeharyung <lhr4884@naver.com>
2023-08-05 16:59:16 +0900

    fix: useGetTotalInfo 변경

--- src/apis/room/query.ts
+++ src/apis/room/query.ts
@@ -1,17 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { httpClient } from '@/apis/http';
-import { type API } from '@/apis/room/type';
+import { type TotalInfo } from '@/apis/room/type';
 
-export const totalInfo = {
-  get: async ({ roomId }: API['totalInfo']['get']['request']) => {
-    const data = await httpClient.get<API['totalInfo']['get']['response']>(
-      `rooms/${roomId}/info`,
+export const useGetTotalInfo = (params: TotalInfo['get']['request']) => {
+  const getTotalInfo = async () => {
+    const data = await httpClient.get<TotalInfo['get']['response']>(
+      `rooms/${params.roomId}/info`,
     );
     return data;
-  },
-};
-
-export const useGetTotalInfo = (params: API['totalInfo']['get']['request']) => {
-  return useQuery(['roomInfo'], () => totalInfo.get({ ...params }));
+  };
+  return useQuery(['roomInfo'], getTotalInfo);
 };
