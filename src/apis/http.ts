d1ee401d85041b00c0fa9d46ecfdd3ef49ef6b53
young-do <yd199323@gmail.com>
2023-08-14 20:02:40 +0900

    추후 sse 연동을 위해 base url도 export

--- src/apis/http.ts
+++ src/apis/http.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const httpClient = {
   get: <T>(url: string) => __fetch<T, void>('get', url),
