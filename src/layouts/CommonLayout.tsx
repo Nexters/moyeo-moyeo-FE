414c44584f99f10c4acdcffb490a98e0632aea04
Young-do Cho <yd199323@gmail.com>
2023-08-15 02:40:04 +0900

    [MOYEO-57] lottie 배경 추가, 홈페이지 + 엔트리 페이지에 적용 (#29)
    * 홈페이지에 lottie 적용
    
    * 로직에 대한 주석 추가
    
    * lottie 배경 사용을 의도적으로 호출
    
    * 로띠가 화면 밖을 나가지 않도록
    
    * params 체크를 paint 이전에 하도록 수정
    
    * 불필요한 변수 제거

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -1,13 +1,39 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { Player } from '@lottiefiles/react-lottie-player';
+import { useAtomValue } from 'jotai';
+import { Outlet } from 'react-router-dom';
 
 import BlurBackgroundImage from '@/assets/images/blur.webp';
-import HomeBackgroundImage from '@/assets/images/home.webp';
+import LottieBackgroundImage from '@/assets/images/lottie.webp';
+import { isShowLottieBackgroundState } from '@/store/atoms';
 import { css } from '@/styled-system/css';
 import { vstack } from '@/styled-system/patterns';
 
 const CommonLayout = () => {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
+  const isShowLottieBackground = useAtomValue(isShowLottieBackgroundState);
+  const [lottieSize, setLottieSize] = useState({ width: 1280, height: 720 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      // @note: lottie 비율이 16:9.
+      // 화면에 꽉차게(= object-fit: cover)와 같은 효과를 주기 위해 아래 라인을 추가함
+      if (window.innerWidth / window.innerHeight > 16 / 9) {
+        setLottieSize({
+          width: window.innerWidth,
+          height: window.innerWidth * (9 / 16),
+        });
+      } else {
+        setLottieSize({
+          width: window.innerHeight * (16 / 9),
+          height: window.innerHeight,
+        });
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div
@@ -23,7 +49,7 @@ const CommonLayout = () => {
     >
       {/* @note: 두 이미지를 가져오는 이유는 페이지 전환시 깜빡이는 느낌을 제거하기 위함 */}
       <img
-        src={HomeBackgroundImage}
+        src={LottieBackgroundImage}
         className={css({
           position: 'absolute',
           top: '0',
@@ -31,9 +57,35 @@ const CommonLayout = () => {
           width: '100%',
           height: '100%',
           objectFit: 'cover',
-          opacity: isHome ? 1 : 0,
+          opacity: isShowLottieBackground ? 1 : 0,
         })}
       />
+      {isShowLottieBackground && (
+        <div
+          className={css({
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+          })}
+        >
+          <Player
+            autoplay
+            loop
+            src="/lottie/star-background.json"
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: lottieSize.width,
+              height: lottieSize.height,
+            }}
+          />
+        </div>
+      )}
       <img
         src={BlurBackgroundImage}
         className={css({
@@ -43,7 +95,7 @@ const CommonLayout = () => {
           width: '100%',
           height: '100%',
           objectFit: 'cover',
-          opacity: isHome ? 0 : 1,
+          opacity: isShowLottieBackground ? 0 : 1,
         })}
       />
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
+import { useLayoutEffect } from 'react';
+
+import { useSetAtom } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 
 import HomeLogo from '@/assets/icons/homeLogo.svg';
+import { isShowLottieBackgroundState } from '@/store/atoms';
 import { css } from '@/styled-system/css';
 import { vstack } from '@/styled-system/patterns';
 
 const Home = () => {
   const navigate = useNavigate();
+  const setIsShowLottieBackground = useSetAtom(isShowLottieBackgroundState);
 
   const handleCreateButton = () => {
     navigate('/create');
   };
+
+  useLayoutEffect(() => {
+    // @note: 이 페이지에서 lottie background를 보여줘야해서 이렇게 씀
+    setIsShowLottieBackground(true);
+    return () => setIsShowLottieBackground(false);
+  }, [setIsShowLottieBackground]);
 
   return (
     <section
