80762a247aac07a40119075f274adc0c46655ff0
young-do <yd199323@gmail.com>
2023-08-16 01:12:08 +0900

    이름 글자수 제한 더 적게

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -41,7 +41,7 @@ export const POSITION_LIST = Object.entries(POSITION).map(([key, value]) => ({
 }));
 
 // max length
-export const MAX_LENGTH__USER_NAME = 50;
+export const MAX_LENGTH__USER_NAME = 20;
 export const MAX_LENGTH__USER_PROFILE = 50;
 
 export const MAX_LENGTH__TEAM_BUILDING_NAME = 20;
