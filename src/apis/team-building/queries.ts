d6d242042a075d0de2022f2d819fb8cb36dda0ca
young-do <yd199323@gmail.com>
2023-08-18 02:57:38 +0900

    쿼리데이터 변경할 수 있는 함수도 추가

--- src/apis/team-building/queries.ts
+++ src/apis/team-building/queries.ts
@@ -1,4 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
+
+import { Updater, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { httpClient } from '@/apis/http';
 import { type API } from '@/apis/type';
@@ -6,12 +8,26 @@ import { type API } from '@/apis/type';
 export const useGetTotalInfo = (
   teamBuildingUuid?: API['getTotalInfo']['request']['teamBuildingUuid'],
 ) => {
-  return useQuery(
+  type Response = API['getTotalInfo']['response'];
+
+  const queryClient = useQueryClient();
+
+  const setTotalInfo = useCallback(
+    (updater: Updater<Response | undefined, Response | undefined>) => {
+      queryClient.setQueryData<Response>(
+        ['totalInfo', teamBuildingUuid],
+        updater,
+      );
+    },
+    [queryClient, teamBuildingUuid],
+  );
+
+  const queryResult = useQuery(
     ['totalInfo', teamBuildingUuid],
     async () => {
       if (!teamBuildingUuid) throw new Error('teamBuildingUuid is required');
 
-      const data = await httpClient.get<API['getTotalInfo']['response']>(
+      const data = await httpClient.get<Response>(
         `/api/team-building/${teamBuildingUuid}`,
       );
       // @fixme: 제거할 것
@@ -22,4 +38,9 @@ export const useGetTotalInfo = (
       enabled: !!teamBuildingUuid,
     },
   );
+
+  return {
+    ...queryResult,
+    setTotalInfo,
+  };
 };
